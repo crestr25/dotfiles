0e85ae98e2adc6c2df4b375923bb0042ec825638
Carlos Restrepo <crestr25@gmail.com>
2022-02-06 17:02:45 -0500

    added changes for new Nvim and Tmux configs

---
+++ README.md
@@ -0,0 +1,10 @@
+# DOTFILES
+My config files for:
+- Nvim
+- Tmux
+- Zsh (To do)
+
+## Installation
+Requires GNU stow   
+`stow */`
+
--- nvim/init.lua
+++ nvim/init.lua
@@ -1,3 +1,28 @@
+-------------------
+-- NEOVIM CONFIG --
+-------------------
 
-require "options"
-require("mappings").default_maps()
+-- BASIC CONFIG
+require "user.options"
+require "user.keymaps"
+require "user.plugins"
+
+-- EDITOR STUFF
+require "user.colorscheme"
+require "user.lualine"
+require "user.nvim-tree"
+require "user.comment"
+require "user.autopairs"
+require "user.bufferline"
+
+-- AUTOCOMPLETION
+require "user.cmp"
+require "user.lsp"
+require "user.telescope"
+require "user.treesitter"
+require "user.gitsigns"
+require "user.toggleterm"
+require "user.impatient"
+require "user.indentline"
+require "user.alpha"
+require "user.autocommands"
