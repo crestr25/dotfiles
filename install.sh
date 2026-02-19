28dd98cbc48afd2695a00e14ced0fe5fd012d26f
Carlos Restrepo <crestr25@gmail.com>
2026-02-18 20:52:02 -0500

    added homebrew and xcode to install sh

--- install.sh
+++ install.sh
@@ -23,3 +23,42 @@ warning() {
 }
 
 info "Initializing dotfiles setup...."
+read -p "Install apps? [y/n] " install_apps
+
+if [[ "$install_apps" == "y" ]]; then
+    printf "\n"
+    info "===================="
+    info "Prerequisites"
+    info "===================="
+
+    info "Installing Apple's CLI tools..."
+    if xcode-select -p >/dev/null; then
+        warning "xcode is already installed"
+    else
+        xcode-select --install
+        sudo xcodebuild -license accept
+    fi
+
+    info "Installing Homebrew..."
+    export HOMEBREW_CASK_OPTS="--appdir=/Applications"
+    if hash brew &>/dev/null; then
+        warning "Homebrew already installed"
+    else
+        sudo --validate
+        NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+    fi
+
+    printf "\n"
+    info "===================="
+    info "Apps"
+    info "===================="
+
+    # install_custom_formulae
+    # install_custom_casks
+    # run_brew_bundle
+else
+    printf "\n"
+    info "===================="
+    info "Ok...... Bye then"
+    info "===================="
+fi
